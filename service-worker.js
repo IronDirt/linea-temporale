246bc47f3de0a1b9d3330f3daad140e20a8cea48
IronDirt <36108392+IronDirt@users.noreply.github.com>
2026-03-21 08:25:29 +0100

    ok2

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,35 @@
 // service-worker.js
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('linea-temporale-v1').then(cache => {
+    caches.open("linea-temporale-v1").then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.php',
-        '/style.css',
-        '/style-mobile.css',
-        '/style-desktop.css',
-        '/app.js',
-        '/manifest.json',
-        '/lang/it.js',
-        '/lang/en.js',
-        '/lang/es.js',
-        '/lang/de.js',
-        '/lang/fr.js',
-        '/lang/pt.js',
-        '/lang/ru.js',
-        '/lang/tr.js',
-        '/lang/ja.js',
-        '/lang/zh.js',
+        "/",
+        "/index.php",
+        "/style.css",
+        "/style-mobile.css",
+        "/style-desktop.css",
+        "/app.js",
+        "/manifest.json",
+        "/lang/it.js",
+        "/lang/en.js",
+        "/lang/es.js",
+        "/lang/de.js",
+        "/lang/fr.js",
+        "/lang/pt.js",
+        "/lang/ru.js",
+        "/lang/tr.js",
+        "/lang/ja.js",
+        "/lang/zh.js",
         // aggiungi qui eventuali altre risorse statiche
       ]);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
